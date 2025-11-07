36524856c6ab3bade052a636ab4df6071c6027d1
FabioSimones <simones.fabio1994@gmail.com>
2025-11-06 22:22:31 -0300

    Projeto criado.

---
+++ src/main/java/com/devfabiosimones/gntech/GntechApplication.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.gntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GntechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GntechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfabiosimones/gntech/GntechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.gntech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GntechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
